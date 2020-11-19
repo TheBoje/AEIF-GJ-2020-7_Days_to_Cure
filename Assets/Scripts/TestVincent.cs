23e70deb5d53cca05ea47263daac5dfd8d95c0c4
Vincent <vincent.commin@etu.univ-poitiers.fr>
2020-11-19 16:46:26 +0100

    Test de rider + test de fit via rider

--- Assets/Scripts/TestVincent.cs
+++ Assets/Scripts/TestVincent.cs
@@ -1,5 +1,14 @@
-﻿
-public class TestVincent : UnityEngine.MonoBehaviour
+﻿using System;
+using UnityEngine;
+public class TestVincent : MonoBehaviour
 {
-    
+    private void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Update()
+    {
+        throw new NotImplementedException();
+    }
 }
