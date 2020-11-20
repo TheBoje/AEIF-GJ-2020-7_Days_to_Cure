90628cf9798e365772a300ac407e28abc689e21f
Vincent <vincent.commin@etu.univ-poitiers.fr>
2020-11-20 23:56:24 +0100

    Ajout de la touche interaction + flash objets

---
+++ Assets/Scripts/Altimor/GlowingObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowingObject : MonoBehaviour
+{
+    public const int redCol = 210;
+    public const int greenCol = 105;
+    public const int blueCol = 10;
+
+    private bool isFlashing;
+
+    private void Start()
+    {
+        isFlashing = false;
+    }
+
+    public void StartFlash()
+    {
+        if (!isFlashing)
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color(redCol, greenCol, blueCol);
+        }
+    }
+
+    public void StopFlash()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Altimor/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private GameObject handleObject;
+    private GameObject selectedObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        handleObject = null;
+    }
+
+    private GameObject CastRay()
+    {
+        return Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out var hit) ? hit.transform.gameObject : null;
+    }
+
+    private void Update()
+    {
+        selectedObject = CastRay();
+        if (selectedObject)
+        {
+            
+        }
+    }
+}
