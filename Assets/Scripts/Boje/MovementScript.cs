418975722f2d083b6cfbc255933251ea93b2c200
Louis Leenart <louis.leenar@gmail.com>
2020-11-20 22:26:33 +0100

    player movement fait

--- Assets/Scripts/Boje/MovementScript.cs
+++ Assets/Scripts/Boje/MovementScript.cs
@@ -1,21 +1,20 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
-    private Vector3 _move;
+    private Vector3 _move = Vector3.zero;
     public float speed = 1f;
     [SerializeField] [Range(0f, 1f)] private float lerpCoef = 0.15f; // [0, 1]
     public new Transform transform;
     public Transform cameraTransform;
-    
+
     void FixedUpdate()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         _move = transform.right * (x * speed) + transform.forward * (z * speed);
+        _move = cameraTransform.TransformDirection(_move);
+        _move.y = 0;
         var position = transform.position;
         position = Vector3.Lerp(position, position + _move, lerpCoef);
         transform.position = position;
