debf895f26dcc76b6e5b293ab5bad88d5354c76c
Vincent <vincent.commin@etu.univ-poitiers.fr>
2020-11-21 18:45:23 +0100

    ajout text mesh pro

--- Assets/Scripts/Altimor/GameManager.cs
+++ Assets/Scripts/Altimor/GameManager.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using Random = System.Random;
 
 public class GameManager : MonoBehaviour
 {
     private const int nbIngredients = 3;
-    
-    [SerializeField] private Dictionary<string, string> ingredients;
+    public Dictionary<string, string> ingredients;
     private Dictionary<string, string> solution;
-    
+
+    public TextMeshPro text;
+    public GameObject player;
+    public GameObject bowl;
+
+    private void Start()
+    {
+        ingredients = new Dictionary<string, string>();
+        
+        ingredients.Add("Test1", "Mmmmmh le bon test");
+        ingredients.Add("Test2", "Encore du test ?");
+        ingredients.Add("Test3", "Relou la en fait");
+    }
+
     public Dictionary<String, String> ChooseAntidote()
     {
         Dictionary<string, string> dic = new Dictionary<string, string>();
@@ -20,9 +34,16 @@ public class GameManager : MonoBehaviour
         for (int i = 0; i < nbIngredients; i++)
         {
             int rand = random.Next(ingredients.Count);
-            
+            dic.Add(ingredients.ElementAt(rand).Key, ingredients.ElementAt(rand).Value);
         }
 
         return dic;
     }
+
+    public void printOnTvScreen()
+    {
+        
+    }
+    
+    
 }
