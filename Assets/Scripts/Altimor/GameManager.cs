6ea0a3a144eee3de74891591918831f78fe70635
Vincent <vincent.commin@etu.univ-poitiers.fr>
2020-11-21 17:57:03 +0100

    Ntm benjamin

---
+++ Assets/Scripts/Altimor/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class GameManager : MonoBehaviour
+{
+    private const int nbIngredients = 3;
+    
+    [SerializeField] private Dictionary<string, string> ingredients;
+    private Dictionary<string, string> solution;
+    
+    public Dictionary<String, String> ChooseAntidote()
+    {
+        Dictionary<string, string> dic = new Dictionary<string, string>();
+        
+        Random random = new Random();
+
+        for (int i = 0; i < nbIngredients; i++)
+        {
+            int rand = random.Next(ingredients.Count);
+            
+        }
+
+        return dic;
+    }
+}
