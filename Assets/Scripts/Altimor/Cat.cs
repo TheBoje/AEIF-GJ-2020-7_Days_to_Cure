56e20224f048af5655cb52680f80fb5168bcf0bc
Vincent <vincent.commin@etu.univ-poitiers.fr>
2020-11-21 23:01:20 +0100

    Ajout de virus et de son son

---
+++ Assets/Scripts/Altimor/Cat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.GetComponent<AudioSource>().playOnAwake = true;
+    }
+
+
+    public void Pet()
+    {
+        transform.GetComponent<AudioSource>().Play();
+    }
+}
