d3b091d43f85bcccf0899fb435fcb17819ea48e1
Vincent <vincent.commin@etu.univ-poitiers.fr>
2020-11-22 16:49:17 +0100

    Ajout de la musique

---
+++ Assets/Scripts/Altimor/Music.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    static Music instance;
+ 
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
