a7051a22f01e71c4f91b2b57460a4473cb9d9a3d
Vincent <vincent.commin@etu.univ-poitiers.fr>
2020-11-22 09:30:14 +0100

    Fix de la télé

--- Assets/Scripts/Altimor/GameManager.cs
+++ Assets/Scripts/Altimor/GameManager.cs
@@ -4,13 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using Random = System.Random;
 
 public class GameManager : MonoBehaviour
 {
+    public static bool firstStart = true;
+    private static string indications = "";
+    
     private const int nbIngredients = 3;
     public Dictionary<string, string> ingredients;
     private Dictionary<string, string> solution;
+
+    
 
     public TextMeshPro text;
     public GameObject player;
@@ -22,33 +29,37 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         DontDestroyOnLoad(this);
-
+       
+        
+        //Debug.Log(indications);
+        //text.text = indications;
+        
         playerScript = player.GetComponent<Player>();
         bowlScript = bowl.GetComponent<BowlBehaviour>();
     }
 
     private void Start()
     {
-        ingredients = new Dictionary<string, string>();
-        solution = new Dictionary<string, string>();
-
-        ingredients.Add("Test1", "Mmmmmh le bon test");
-        ingredients.Add("Test2", "Encore du test ?");
-        ingredients.Add("Test3", "Relou la en fait");
-        ingredients.Add("Test4", "Relou la en fait");
-        ingredients.Add("Test5", "Relou la en fait");
-        ingredients.Add("Test6", "Relou la en fait");
-        ingredients.Add("Test7", "Relou la en fait");
-        ingredients.Add("Test8", "Relou la en fait");
+        text.SetText(indications);
+        if (firstStart)
+        {
+            ingredients = new Dictionary<string, string>();
+            solution = new Dictionary<string, string>();
+            
+            ingredients.Add("Test1", "Mmmmmh le bon test");
+            ingredients.Add("Test2", "Encore du test ?");
+            ingredients.Add("Test3", "Relou la en fait");
+            ingredients.Add("Test4", "Relou la en fait");
+            ingredients.Add("Test5", "Relou la en fait");
+            ingredients.Add("Test6", "Relou la en fait");
+            ingredients.Add("Test7", "Relou la en fait");
+            ingredients.Add("Test8", "Relou la en fait");
         
-        ChooseAntidote();
-        
-        /*solution.Add("Test1", "Mmmmmh le bon test");
-        solution.Add("Test2", "Encore du test ?");
-        solution.Add("Test3", "Relou la en fait");
-        */
-        
-        printIndicationOnTvScreen();
+            ChooseAntidote();
+            ComputeIndications();
+            text.SetText(indications);
+            firstStart = false;
+        }
     }
 
     public void ChooseAntidote()
@@ -68,21 +79,23 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void printIndicationOnTvScreen()
+    public void ComputeIndications()
     {
-        text.text = "On sait que le virus SHRARS-32 n'aime pas : \n";
+        indications = "On sait que le virus SHRARS-32 n'aime pas : \n";
         foreach (var key in solution)
         {
-            text.text += "- " + key.Value + "\n";
+            indications += "- " + key.Value + "\n";
         }
 
-        text.text += "Vous avez ajouté :\n";
+        indications += "Vous avez ajouté :\n";
     }
 
     public void nextDay()
     {
         // reload la scene an gardant le game manager et le calendrier ? avec un fondu au noir ?
-        
+        Destroy(text.gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
     }
 
     public void PrintIngredient(string ingredientName)
